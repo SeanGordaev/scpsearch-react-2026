d7707e6b946d06fd38bbac36584122bd69f63804
GitGor <131777823+SeanGordaev@users.noreply.github.com>
2026-03-01 01:36:53 +0200

    start with search page

---
+++ src/pages/SE.js
@@ -0,0 +1,17 @@
+import '../style/SE.css'
+import logo from '../imgs/scp-logo-se.png'
+
+export const SE = (props) => {
+    return (
+        <div className='se-display'>
+            {/* <img id='img-logo' src={logo} alt='Who are you?' width="300px" height="auto"/> */}
+
+            <div className='search-pleace'>
+                <label>SCP</label>
+                <label>-</label>
+                <input/>
+            </div>
+
+        </div>
+    )
+} 
