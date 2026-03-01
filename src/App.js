5acd89edf7d6badc9e8bd2f78b005b0475f6b878
GitGor <131777823+SeanGordaev@users.noreply.github.com>
2026-03-02 00:29:59 +0200

    WP

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 import { Home } from './pages/Home.js';
 import { SE } from './pages/SE.js';
+import { Data } from './components/data.js';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<div className='app'><Home/></div>}/>
-        <Route path='/SE' element={<SE/>}/>
-      </Routes>
-    </BrowserRouter>
+    <Data>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<div className='app'><Home/></div>}/>
+          <Route path='/SE' element={<SE/>}/>
+          <Route path='/SE' element={<SE/>}/>
+        </Routes>
+      </BrowserRouter>
+    </Data>
   );
 }
 
---
+++ src/pages/WormProtocol.js
@@ -0,0 +1,7 @@
+export const WP = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+} 
