84731e9b8743f6156ec1020d434c3754a69d42f1
GitGor <131777823+SeanGordaev@users.noreply.github.com>
2026-03-01 17:03:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 import { Home } from './pages/Home.js';
 import { SE } from './pages/SE.js';
 import './App.css';
-import { useState } from 'react';
 
 function App() {
   return (
