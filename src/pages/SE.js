61816cfc6cb6d4483e7db2421e0177fd4ab06b74
GitGor <131777823+SeanGordaev@users.noreply.github.com>
2026-03-01 14:39:13 +0200

    Make And Visual Search page

--- src/pages/SE.js
+++ src/pages/SE.js
@@ -1,15 +1,38 @@
 import '../style/SE.css'
 import logo from '../imgs/scp-logo-se.png'
+import { ReactTyped as Typed } from 'react-typed'
+import { useState } from 'react'
 
 export const SE = (props) => {
+    const [show, setShow] = useState(false);
+    
+    const showSearch = () => {
+        setShow(true);
+    }
+
     return (
         <div className='se-display'>
-            {/* <img id='img-logo' src={logo} alt='Who are you?' width="300px" height="auto"/> */}
+            <img id='img-logo' src={logo} alt='Who are you?' width="300px" height="auto"/>
+            
+            
 
             <div className='search-pleace'>
-                <label>SCP</label>
-                <label>-</label>
-                <input/>
+                <div>
+                    <label id='title'>
+                        <Typed
+                            strings={["Secure. Contain. Protect."]}
+                            typeSpeed={100}
+                            onStringTyped={showSearch}
+                            showCursor={false}
+                        />
+                    </label>
+                </div>
+                {show ? <div className='search-input'>
+                    <label>SCP</label>
+                    <label>-</label>
+                    <input type=''/>
+                    <button>Found</button>
+                </div> : <></>}
             </div>
 
         </div>
